cb22af1b0b3e1c915810432fbceb047b55a37353
NikolaosKantartzopoulos <nkantartzopoulos@gmail.com>
2024-05-14 17:16:14 +0300

    Group Anagrams

---
+++ src/main/java/problems/GroupAnagrams.java
@@ -0,0 +1,26 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+
+            map.get(sorted).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
---
+++ src/test/groovy/GroupAnagramsSpec.groovy
@@ -0,0 +1,22 @@
+import problems.GroupAnagrams
+import spock.lang.Specification
+
+class GroupAnagramsSpec extends Specification {
+
+    static List<List<String>> sortLists(List<List<String>> lists) {
+        lists.collect { it.sort() }.sort()
+    }
+
+    def "Group Anagrams"() {
+        expect:
+            sortLists(GroupAnagrams.groupAnagrams(strs)) == sortLists(output)
+
+        where:
+            strs                    | output
+            ["eat","tea","tan","ate","nat","bat"] as String[] | [["bat"],["nat","tan"],["ate","eat","tea"]]
+            [""] as String[]                                | [[""]]
+            ["a"] as String[]                               | [["a"]]
+
+
+    }
+}
