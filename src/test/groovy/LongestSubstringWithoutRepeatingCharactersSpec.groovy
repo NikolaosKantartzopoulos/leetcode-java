e7dd78fda3f1c9202515a52ec98a32dddd0ead8b
NikolaosKantartzopoulos <nkantartzopoulos@gmail.com>
2024-05-17 12:49:24 +0300

    3. Longest Substring Without Repeating Characters

---
+++ src/test/groovy/LongestSubstringWithoutRepeatingCharactersSpec.groovy
@@ -0,0 +1,14 @@
+import problems.LongestSubstringWithoutRepeatingCharacters
+import spock.lang.Specification
+
+class LongestSubstringWithoutRepeatingCharactersSpec extends Specification{
+    def "Longest Substring Without Repeating Characters" () {
+        expect:
+            LongestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring(s) == result
+        where:
+            s           | result
+//            "abcabcbb"  | 3
+            "bbbbb"     | 1
+//            "pwwkew"    | 3
+    }
+}
