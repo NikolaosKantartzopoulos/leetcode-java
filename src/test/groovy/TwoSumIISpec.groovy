7571120eafd53f301a788645ad459fa43d752ec4
NikolaosKantartzopoulos <nkantartzopoulos@gmail.com>
2024-05-17 11:19:48 +0300

    Fix Two Sum II tests

--- src/test/groovy/TwoSumIISpec.groovy
+++ src/test/groovy/TwoSumIISpec.groovy
@@ -1,14 +1,14 @@
-import problems.TwoSum
+import problems.TwoSumII
 import spock.lang.Specification
 
 class TwoSumIISpec extends Specification{
     def"Two Sum II"(){
         expect:
-            TwoSum.twoSum(numbers, target) == findResult
+            TwoSumII.twoSum(numbers, target) == findResult
         where:
-            numbers     | target | result
-            [2,7,11,15] | 9      | [1,2]
-            [2,3,4]     | 6      | [1,3]
-            [-1,0]      | 1      | [1,2]
+            numbers     | target | findResult
+            [2,7,11,15] as int [] | 9 as int | [1,2] as int[]
+            [2,3,4]     as int [] | 6 as int | [1,3] as int[]
+            [-1,0]      as int [] | -1 as int | [1,2] as int[]
     }
 }
